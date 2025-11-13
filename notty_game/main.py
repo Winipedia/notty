f6aca7b3f5bb1f87667f70a0a216947ab6b26906
Winipedia <win.steveker@gmx.de>
2025-11-13 02:43:02 +0000

    test fix

--- notty_game/main.py
+++ notty_game/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+"""main.py: main module.
+
+This module contains the main function and entry point for the application.
+"""
+
+
+def main() -> None:
+    """Main function.
+
+    This function is the entry point for the application.
+    """
