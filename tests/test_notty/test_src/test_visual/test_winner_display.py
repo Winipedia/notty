49fefb0c8078a50dd0cd0be815d8720590b5c63e
Winipedia <win.steveker@gmx.de>
2025-11-23 04:53:06 +0000

    winner screen added

--- tests/test_notty/test_src/test_visual/test_winner_display.py
+++ tests/test_notty/test_src/test_visual/test_winner_display.py
@@ -4,6 +4,9 @@
 class TestWinnerDisplay:
     """Test class."""
 
+    def test__draw_button(self) -> None:
+        """Test method."""
+
     def test___init__(self) -> None:
         """Test method."""
 
