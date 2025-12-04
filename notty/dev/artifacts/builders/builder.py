b8e93b880c84f60678786213c1c7532b12d1b37c
Winipedia <win.steveker@gmx.de>
2025-12-04 16:54:35 +0000

    fix

---
+++ notty/dev/artifacts/builders/builder.py
@@ -0,0 +1,17 @@
+"""Build script.
+
+All subclasses of Builder in the builds package are automatically called.
+"""
+
+from types import ModuleType
+
+from pyrig.dev.artifacts.builders.base.base import PyInstallerBuilder
+
+
+class NottyBuilder(PyInstallerBuilder):
+    """Builder for notty."""
+
+    @classmethod
+    def get_additional_resource_pkgs(cls) -> list[ModuleType]:
+        """Get the add datas."""
+        return []
