ccc84452afa2013fdb45c6eb72e0cf7d35a056d3
Winipedia <winipedia@gmx.de>
2025-12-31 16:54:20 +0100

    pyrig update

---
+++ notty/dev/tests/mirror_test.py
@@ -0,0 +1,24 @@
+"""Subclassing MirrorTest to test the notty game."""
+
+from pyrig.dev.tests.mirror_test import MirrorTestConfigFile
+
+
+class NottyMirrorTestConfigFile(MirrorTestConfigFile):
+    """Subclassing MirrorTest to test the notty game."""
+
+    @classmethod
+    def get_test_func_skeleton(cls, test_func_name: str) -> str:
+        """Get the test function skeleton."""
+        return f'''
+
+def {test_func_name}() -> None:
+    """Test function."""
+'''
+
+    @classmethod
+    def get_test_method_skeleton(cls, test_method_name: str) -> str:
+        """Get the test method skeleton."""
+        return f'''
+    def {test_method_name}(self) -> None:
+        """Test method."""
+'''
---
+++ tests/test_notty/test_dev/test_tests/test_mirror_test.py
@@ -0,0 +1,11 @@
+"""module."""
+
+
+class TestNottyMirrorTestConfigFile:
+    """Test class."""
+
+    def test_get_test_func_skeleton(self) -> None:
+        """Test method."""
+
+    def test_get_test_method_skeleton(self) -> None:
+        """Test method."""
