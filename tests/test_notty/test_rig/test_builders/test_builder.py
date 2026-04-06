d7d4d80a34a88ee9c8efe0110d79ce7ec1b265a4
Winipedia <winipedia@gmx.de>
2026-04-06 18:07:42 +0100

    pyrig udate

--- tests/test_notty/test_rig/test_builders/test_builder.py
+++ tests/test_notty/test_rig/test_builders/test_builder.py
@@ -4,5 +4,8 @@
 class TestNottyBuilder:
     """Test class."""
 
+    def test_app_icon_png_location(self) -> None:
+        """Test method."""
+
     def test_entry_point_module(self) -> None:
         """Test method."""
