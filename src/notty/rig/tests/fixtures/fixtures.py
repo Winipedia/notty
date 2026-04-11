007a0240ba7a151547773a12d6ac5a7c81b6d1aa
Winipedia <winipedia@gmx.de>
2026-04-11 13:31:03 +0100

    update pyrig

--- src/notty/rig/tests/fixtures/fixtures.py
+++ src/notty/rig/tests/fixtures/fixtures.py
@@ -1,12 +1,15 @@
 """Defines pytest fixtures."""
 
 import pytest
+from pyrig.rig.tests.fixtures.autouse.session import (
+    package_manager_updated as pyrig_package_manager_updated,
+)
 
 override_works = False
 
 
 @pytest.fixture(scope="session")
-def assert_package_manager_is_up_to_date() -> None:
+def package_manager_updated() -> None:
     """Overrides pyrigs fixture of the same name to check overriding works correctly."""
     global override_works  # noqa: PLW0603
     override_works = True
@@ -14,9 +17,10 @@ def assert_package_manager_is_up_to_date() -> None:
 
 
 @pytest.fixture(scope="session", autouse=True)
-def assert_override_works(assert_package_manager_is_up_to_date: None) -> None:
+def assert_override_works(package_manager_updated: None) -> None:
     """Checks that the previous fixture override worked as expected."""
-    assert assert_package_manager_is_up_to_date is None, (
+    assert pyrig_package_manager_updated.__name__ == "package_manager_updated"
+    assert package_manager_updated is None, (
         "Previous fixture override did not run as expected"
     )
     assert override_works, "Previous fixture override did not work as expected"
--- tests/test_notty/test_rig/test_tests/test_fixtures/test_fixtures.py
+++ tests/test_notty/test_rig/test_tests/test_fixtures/test_fixtures.py
@@ -5,5 +5,5 @@ def test_assert_override_works() -> None:
     """Test function."""
 
 
-def test_assert_package_manager_is_up_to_date() -> None:
+def test_package_manager_updated() -> None:
     """Test function."""
